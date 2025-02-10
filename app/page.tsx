46f6af00d60aafbcce4855d1347867af7c6440ca
tewarishash12 <tewarishashwat2002@gmail.com>
2025-02-11 00:54:11 +0530

    fixed the lottie-react issue

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
+import dynamic from "next/dynamic";
 import Education from "@/components/Education";
 import Footer from "@/components/Footer";
-import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
 import TechStack from "@/components/TechStack";
 import { FloatingNav } from "@/components/ui/floating-navbar";
 import { navItems } from "@/data";
+
+const Grid = dynamic(() => import("@/components/Grid"), { ssr: false });
   
 export default function Home() {
   return (
